923b2b9bcab28c2486b82d262b5f35dcfdbfa10c
Evan Tseng <evan@tseng.io>
2016-08-27 16:55:13 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# rpi3-pinout
+The [Raspberry Pi][rpi] 3 pinout.
+![Raspberry Pi Pin Map](https://www.element14.com/community/servlet/JiveServlet/previewBody/73950-102-10-339300/pi3_gpio.png)
+
+[rpi]: https://www.raspberrypi.org
---
+++ index.js
@@ -0,0 +1,133 @@
+'use strict';
+
+module.exports = {
+  p1: {
+    power: '3.3v'
+  },
+  p2: {
+    power: '5v'
+  },
+  p3: {
+    gpio: 2
+  },
+  p4: {
+    power: '5v'
+  },
+  p5: {
+    gpio: 3
+  },
+  p6: {
+    power: 'ground'
+  },
+  p7: {
+    gpio: 4
+  },
+  p8: {
+    gpio: 14,
+    uart: {
+      type: 'tx',
+      index: 0
+    }
+  },
+  p9: {
+    power: 'ground'
+  },
+  p10: {
+    gpio: 15,
+    uart: {
+      type: 'rx',
+      index: 0
+    }
+  },
+  p11: {
+    gpio: 17,
+    pwm: 0
+  },
+  p12: {
+    gpio: 18,
+    pwm: 1
+  },
+  p13: {
+    gpio: 27,
+    pwm: 2
+  },
+  p14: {
+    power: 'ground'
+  },
+  p15: {
+    gpio: 22,
+    pwm: 3
+  },
+  p16: {
+    gpio: 23,
+    pwm: 4
+  },
+  p17: {
+    power: '3.3v'
+  },
+  p18: {
+    gpio: 24,
+    pwm: 5
+  },
+  p19: {
+    gpio: 10
+  },
+  p20: {
+    power: 'ground'
+  },
+  p21: {
+    gpio: 9
+  },
+  p22: {
+    gpio: 25,
+    pwm: 6
+  },
+  p23: {
+    gpio: 11
+  },
+  p24: {
+    gpio: 8
+  },
+  p25: {
+    power: 'ground'
+  },
+  p26: {
+    gpio: 7
+  },
+  p29: {
+    gpio: 5
+  },
+  p30: {
+    power: 'ground'
+  },
+  p31: {
+    gpio: 6
+  },
+  p32: {
+    gpio: 12
+  },
+  p33: {
+    gpio: 13
+  },
+  p34: {
+    power: 'ground'
+  },
+  p35: {
+    gpio: 19
+  },
+  p36: {
+    gpio: 16
+  },
+  p37: {
+    gpio: 26
+  },
+  p38: {
+    gpio: 20
+  },
+  p39: {
+    power: 'ground'
+  },
+  p40: {
+    gpio: 21
+  }
+};
